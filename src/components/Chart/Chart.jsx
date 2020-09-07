8486adb126628c515224338052219c55b554ab95
vsreyes <virgilio.s.reyes@gmail.com>
2020-09-07 19:20:50 +0200

    Added CountryPicker.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -50,7 +50,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                         borderColor:'#3333ff',
                         fill: true,
                     }, {
-                        data.dailyData.map((data) => data.deaths),
+                        data: dailyData.map((data) => data.deaths),
                         label: 'Deaths',
                         borderColor: 'red',
                         backgroundColor: 'rgba(255, 0, 0, 0.5)',
