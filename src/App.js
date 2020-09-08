13703f8532c00f3d6b95c510dc81f8d0b274844e
vsreyes <virgilio.s.reyes@gmail.com>
2020-09-08 16:04:56 +0200

    Added corrections

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
         country: '',
     }
 
-    aync componentDidMount() {
+    async componentDidMount() {
         const data = await fetchData();
 
         this.setState({ data });
